d182775e099d7691986b538712873f26e214973b
Onionie <gutierrez.onionie@gmail.com>
2021-10-12 23:53:39 -0700

    Started Working on Mapping components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes.js";
 
 function App(){
     return <div>
         <Header />
-        <Note />
+        <Note 
+            Title = {notes.title}
+            Content = {notes.content}
+
+        />
         <Footer />
     </div>
 }
