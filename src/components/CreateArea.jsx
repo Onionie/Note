91250320acb0356649cbfb30e2e039c45504f8b5
Onionie <gutierrez.onionie@gmail.com>
2021-10-18 21:38:01 -0700

    Working on Creating a note area

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 
 function CreateArea() {
+
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+
+        setNote(prevNote => {
+            return {
+                ...prevNote,
+                [name]: value
+            };
+        });
+    }
+
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
+        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
         <textarea name="content" placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
