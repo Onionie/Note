2e98c73ca375d213667695580e55fa1b22cfb788
Onionie <gutierrez.onionie@gmail.com>
2021-10-18 23:54:20 -0700

    Working on Adding previous notes

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
 
     const [note, setNote] = useState({
         title: "",
@@ -18,13 +18,23 @@ function CreateArea() {
         });
     }
 
+    function submitNote(event) {
+        props.onAdd(note);
+        setNote({
+            title: "",
+            content: ""
+        });
+        // stops reloading of the page
+        event.preventDefault();
+    }
+
 
   return (
     <div>
       <form>
         <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note..." rows="3" />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
