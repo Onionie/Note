aad7cdd69b59f44806230c5a94acf5d87307c272
Onionie <gutierrez.onionie@gmail.com>
2021-10-19 22:27:28 -0700

    Updated Add/Delete Icons using materials

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom'
 
 function CreateArea(props) {
 
@@ -31,11 +34,13 @@ function CreateArea(props) {
 
   return (
     <div>
-      <form className="create-note">
-        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
-        <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note..." rows="3" />
-        <button onClick={submitNote}>Add</button>
-      </form>
+        <form className="create-note">
+            <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
+            <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note..." rows="3" />
+            <Zoom in={true}>
+            <Fab onClick={submitNote}><AddIcon /></Fab>
+            </Zoom>
+        </form>
     </div>
   );
 }
